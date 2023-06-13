8755fa941cca417ef5ef51bbbaedabdcd7cd9b68
anys34 <anyeseong34@gmail.com>
2023-06-14 01:37:39 +0900

    ADD :: 멤버 변수 생성 - name, tel, nativeSC, score, attendance, point 멤버 변수 생성

--- src/student/Student.java
+++ src/student/Student.java
@@ -1,4 +1,10 @@
 package student;
 
 public class Student {
+    private String name; // 이름
+    private String tel; // 전화번호
+    private String nativeSC; // 출신 중학교
+    private int score; // 성적
+    private int attendance; // 출결
+    private int point; // 가산점
 }
