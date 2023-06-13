ee46a784226d632852b25440ea4d8a0178653d30
anys34 <anyeseong34@gmail.com>
2023-06-14 01:40:40 +0900

    ADD :: Getter and Setter - name, tel, nativeSC, score, attendance, point의 Getter and Setter 추가

--- src/student/Student.java
+++ src/student/Student.java
@@ -7,4 +7,52 @@ public class Student {
     private int score; // 성적
     private int attendance; // 출결
     private int point; // 가산점
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getNativeSC() {
+        return nativeSC;
+    }
+
+    public void setNativeSC(String nativeSC) {
+        this.nativeSC = nativeSC;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public int getAttendance() {
+        return attendance;
+    }
+
+    public void setAttendance(int attendance) {
+        this.attendance = attendance;
+    }
+
+    public int getPoint() {
+        return point;
+    }
+
+    public void setPoint(int point) {
+        this.point = point;
+    }
 }
