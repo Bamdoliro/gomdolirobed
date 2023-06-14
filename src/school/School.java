87eff9d36a11dcbe4ed8b453c3a669167f6d4d79
anys34 <anyeseong34@gmail.com>
2023-06-14 19:01:35 +0900

    ADD :: 원서 조회 - School클래스에서 search메서드 생성 - sort함수 안에 람다식 사용하여 성적 정렬

--- src/school/School.java
+++ src/school/School.java
@@ -3,11 +3,19 @@ package school;
 import student.Student;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class School {
     static ArrayList<Student> studentsData = new ArrayList<>();
 
     public static void submit(ArrayList<Student> obj) {
         if (obj != null) studentsData.addAll(obj);
+    }
+
+    public static void search() {
+        studentsData.sort((student1, student2) -> Integer.compare(student2.getScore(), student1.getScore()));
+        for(Student student : studentsData) {
+            System.out.println(student);
+        }
     }
 }
