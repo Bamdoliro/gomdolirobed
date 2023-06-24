1035b5bace68a5ded3cb7b4763e16e8945c848ec
kangsiwoo <bssmkangsiwoo@gmail.com>
2023-06-24 20:21:32 +0900

    TEST :: API 테스트 통과
    테스트에 진심인편

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,3 @@
-import java.util.ArrayList;
-import java.util.Scanner;
-
 public class Test {
     static class BSSM {
         static class Applicant {
@@ -11,71 +8,89 @@ public class Test {
             private int attendance;
             private int addition;
 
-
-            public Applicant(String name, String phoneNumber, String middleSchool,
-                             int grade, int attendance, int addition) { //
-                this.name = name;
-                this.phoneNumber = phoneNumber;
-                this.middleSchool = middleSchool;
-                this.grade = grade;
-                this.attendance = attendance;
-                this.addition = addition;
-            }
-
 //          S: API
-            public void setName(String name) {
-                this.name = name;
-            }
+            public void setName(String name) {this.name = name;}
 
-            public void setPhoneNumber(String phoneNumber) {
-                this.phoneNumber = phoneNumber;
-            }
+            public void setPhoneNumber(String phoneNumber) {this.phoneNumber = phoneNumber;}
 
-            public void setMiddleSchool(String middleSchool) {
-                this.middleSchool = middleSchool;
-            }
+            public void setMiddleSchool(String middleSchool) {this.middleSchool = middleSchool;}
 
-            public void setGrade(int grade) {
-                this.grade = grade;
-            }
+            public void setGrade(int grade) {this.grade = grade;}
 
-            public void setAttendance(int attendance) {
-                this.attendance = attendance;
-            }
+            public void setAttendance(int attendance) {this.attendance = attendance;}
 
-            public void setAddition(int addition) {
-                this.addition = addition;
-            }
+            public void setAddition(int addition) {this.addition = addition;}
 
-            public String getName() {
-                return name;
-            }
+            public String getName() {return name;}
 
-            public String getPhoneNumber() {
-                return phoneNumber;
-            }
+            public String getPhoneNumber() {return phoneNumber;}
 
-            public String getMiddleSchool() {
-                return middleSchool;
-            }
+            public String getMiddleSchool() {return middleSchool;}
 
-            public int getGrade() {
-                return grade;
-            }
+            public int getGrade() {return grade;}
 
-            public int getAttendance() {
-                return attendance;
-            }
+            public int getAttendance() {return attendance;}
 
-            public int getAddition() {
-                return addition;
-            }
-            //          E: API
+            public int getAddition() {return addition;}
+//          E: API
 
         }
     }
 
     public static void main(String[] args) {
-        BSSM bssm = new BSSM();
+
+        BSSM.Applicant applicant = new BSSM.Applicant();
+
+//      S: Test
+        String name = "강시우";
+        String phoneNumber = "010-5357-****";
+        String middleSchool = "Gyeongnam middle school";
+        int grade = 110;
+        int attendance = 18;
+        int addition = 0;
+
+        applicant.setName(name);
+        applicant.setPhoneNumber(phoneNumber);
+        applicant.setMiddleSchool(middleSchool);
+        applicant.setGrade(grade);
+        applicant.setAttendance(attendance);
+        applicant.setAddition(addition);
+
+        if (applicant.getName().equals(name)) {
+            System.out.println("name:\t\t\t ✔️");
+        }else {
+            System.out.println("name:\t\t\t ❌");
+        }
+
+        if (applicant.getPhoneNumber().equals(phoneNumber)) {
+            System.out.println("phoneNumber:\t ✔️");
+        }else {
+            System.out.println("phoneNumber:\t ❌");
+        }
+
+        if (applicant.getMiddleSchool().equals(middleSchool)) {
+            System.out.println("middleSchool:\t ✔️");
+        }else {
+            System.out.println("middleSchool:\t ❌");
+        }
+
+        if (applicant.getGrade() == grade) {
+            System.out.println("grade:\t\t\t ✔️");
+        }else {
+            System.out.println("grade:\t\t\t ❌");
+        }
+
+        if (applicant.getAttendance() == attendance) {
+            System.out.println("attendance:\t\t ✔️");
+        }else {
+            System.out.println("attendance:\t\t ❌");
+        }
+        if (applicant.getAddition() == addition) {
+            System.out.println("addition:\t\t ✔️");
+        }else {
+            System.out.println("addition:\t\t ❌");
+        }
+//      E: Test
+
     }
 }
