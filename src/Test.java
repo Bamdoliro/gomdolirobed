7a4399fcb87212d0c6222482f570e9a6a9c6c483
kangsiwoo <bssmkangsiwoo@gmail.com>
2023-06-24 20:52:47 +0900

    TEST :: ADD :: setIntegrated

--- src/Test.java
+++ src/Test.java
@@ -21,6 +21,16 @@ public class Test {
 
             public void setAddition(int addition) {this.addition = addition;}
 
+            public void setIntegrated // 통합본
+                    (String name, String phoneNumber, String middleSchool, int grade, int attendance, int addition) {
+                this.name = name;
+                this.phoneNumber = phoneNumber;
+                this.middleSchool = middleSchool;
+                this.grade = grade;
+                this.attendance = attendance;
+                this.addition = addition;
+            }
+
             public String getName() {return name;}
 
             public String getPhoneNumber() {return phoneNumber;}
@@ -55,6 +65,46 @@ public class Test {
         applicant.setGrade(grade);
         applicant.setAttendance(attendance);
         applicant.setAddition(addition);
+
+        if (applicant.getName().equals(name)) {
+            System.out.println("name:\t\t\t ✔️");
+        }else {
+            System.out.println("name:\t\t\t ❌");
+        }
+
+        if (applicant.getPhoneNumber().equals(phoneNumber)) {
+            System.out.println("phoneNumber:\t ✔️");
+        }else {
+            System.out.println("phoneNumber:\t ❌");
+        }
+
+        if (applicant.getMiddleSchool().equals(middleSchool)) {
+            System.out.println("middleSchool:\t ✔️");
+        }else {
+            System.out.println("middleSchool:\t ❌");
+        }
+
+        if (applicant.getGrade() == grade) {
+            System.out.println("grade:\t\t\t ✔️");
+        }else {
+            System.out.println("grade:\t\t\t ❌");
+        }
+
+        if (applicant.getAttendance() == attendance) {
+            System.out.println("attendance:\t\t ✔️");
+        }else {
+            System.out.println("attendance:\t\t ❌");
+        }
+        if (applicant.getAddition() == addition) {
+            System.out.println("addition:\t\t ✔️");
+        }else {
+            System.out.println("addition:\t\t ❌");
+        }
+
+        applicant.setIntegrated
+                (name + " a", phoneNumber + " a", middleSchool + " a", grade+1,
+                        attendance+1, addition+1);
+        System.out.println("\nchange all\n");
 
         if (applicant.getName().equals(name)) {
             System.out.println("name:\t\t\t ✔️");
